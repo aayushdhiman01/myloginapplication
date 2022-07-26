07a0b908064184ff1b121c81da370d418f6aa03b
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-26 10:27:42 +0530

    Update ForgotPasswordActivity.kt

--- app/src/main/java/com/aayush/myapplication/ForgotPasswordActivity.kt
+++ app/src/main/java/com/aayush/myapplication/ForgotPasswordActivity.kt
@@ -2,10 +2,18 @@ package com.aayush.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    lateinit var etEmail:EditText
+    lateinit var btnForgetPassword:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        etEmail=findViewById(R.id.etEmail)
+        btnForgetPassword=findViewById(R.id.tvForgot)
+
+        btnForgetPassword.setOnClickListener {  }
     }
 }
