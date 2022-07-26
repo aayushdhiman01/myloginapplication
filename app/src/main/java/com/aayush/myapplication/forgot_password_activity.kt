7f0ffd726a3455ae6b58d9a5d0673c00ef328c07
aayushdhiman01 <50516628+aayushdhiman01@users.noreply.github.com>
2022-07-27 00:01:13 +0530

    changes

--- app/src/main/java/com/aayush/myapplication/forgot_password_activity.kt
+++ app/src/main/java/com/aayush/myapplication/forgot_password_activity.kt
@@ -8,20 +8,25 @@ import android.widget.TextView
 
 class forgot_password_activity : AppCompatActivity() {
     lateinit var etEmail:EditText
-    lateinit var btnForgetPassword:TextView
+    lateinit var btnGET_OTP:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forgotpassword)
-        etEmail=findViewById(R.id.etEmail)
-        btnForgetPassword=findViewById(R.id.tvForgot)
+        etEmail=findViewById(R.id.etEmail1)
+        btnGET_OTP=findViewById(R.id.etlogin2)
 
-        btnForgetPassword.setOnClickListener {
+        btnGET_OTP.setOnClickListener {
             if(etEmail.text.toString().isNullOrEmpty()){
+                etEmail.error=resources.getString(R.string.please_enter_name)
+                etEmail.requestFocus()
+            }
+            else if(!etEmail.equals("aayushdhiman@gmail.com")){
                 etEmail.error=resources.getString(R.string.please_enter_name)
                 etEmail.requestFocus()
             }
             else{
                 var intent=Intent(this,otp_activity::class.java)
+                intent.putExtra("emailId", etEmail.text.toString())
                 startActivity(intent)
                 finish()
             }
